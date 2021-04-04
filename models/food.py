bcd0e5a0849f0988dffc8408d243fda7a122e2bb
shaniah <shaniah.nizzar@hotmail.com>
2021-04-04 16:33:45 -0700

    adding food

---
+++ models/food.py
@@ -0,0 +1,33 @@
+import itertools
+import pygame
+import random
+import pygame.locals
+
+
+class Apple(pygame.sprite.Sprite):
+    def __init__(self):
+        super().__init__()
+        image = pygame.image.load('apple.png')
+        self.image = pygame.transform.scale(image, (40, 65))
+        self.image.set_colorkey((255, 255, 255))
+        self.rect = self.image.get_rect()
+        self.rect.x = round(random.randrange(0, 1000 - snake_block) / 10.0) * 10.0
+
+
+    def update(self):
+        self.rect.y = round(random.randrange(0, 1000 - snake_block) / 10.0) * 10.0
+        self.rect.x = round(random.randrange(0, 1000 - snake_block) / 10.0) * 10.0
+
+
+class Poison(pygame.sprite.Sprite):
+    def __init__(self):
+        super().__init__()
+        image = pygame.image.load('poison.png')
+        self.image = pygame.transform.scale(image, (40, 65))
+        self.image.set_colorkey((255, 255, 255))
+        self.rect = self.image.get_rect()
+        self.rect.x = round(random.randrange(0, 1000 - snake_block) / 10.0) * 10.0
+
+    def update(self):
+        self.rect.y = round(random.randrange(0, 1000 - snake_block) / 10.0) * 10.0
+        self.rect.x = round(random.randrange(0, 1000 - snake_block) / 10.0) * 10.0
