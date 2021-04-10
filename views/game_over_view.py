295881ffbd3ea744181a81c9afd2807be04d6c5b
akinoFu <akino.fu85@outlook.com>
2021-04-09 21:01:59 -0700

    Modify game over view layout

--- views/game_over_view.py
+++ views/game_over_view.py
@@ -3,38 +3,43 @@ import pygame
 class GameOverView:
     def __init__(self, window):
         self._window = window
-        self.start_btn_pos = [120, 400] # [x, y]
-        self.end_btn_pos = [360, 400]   # [x, y]
-        self.surface_pos = [200, 200]   # [x, y]
-        self.button_size = [120, 100]   # [width, height]
+        self._win_w = window.get_width()
+        self._win_h = window.get_height()
+        self.width = self._win_w * 0.8
+        self.height = self._win_h * 0.8
+        self.surface = pygame.Surface((self.width, self.height))
+
+        self.surface_pos = [self._win_w * 0.1, self._win_h * 0.1]                # [x, y]  
+        self._button_size = [self.width * 0.2, self.height * 0.2]                # [width, height]
+        self._img = pygame.image.load("game_over.png")
+        
 
 
     def display(self):
-        gameover_surface = pygame.Surface((600, 600))
-        gameover_surface.fill((255, 204, 204))
-        
-        title_font = pygame.font.SysFont('Ariel Rounded MT', 60)
+        self.surface.fill((218, 197, 45))
+
         btn_font = pygame.font.SysFont('Ariel Rounded MT', 30)
-        
-        # Game Over Text
-        text_game_over = title_font.render("You lost!", True, (1, 1, 1))
+
+        # Game Over position
+        img_rect = self._img.get_rect(center = (self._win_w/2, self._win_h/3))
         
         # Restart button
-        pygame.draw.rect(gameover_surface, (1, 1, 1), (self.start_btn_pos[0], self.start_btn_pos[1], self.button_size[0], self.button_size[1]))
+        start_rect_pos = [self.width * 0.2, self.height * 0.7]
+        continue_rect = pygame.draw.rect(self.surface, (1, 1, 1), (start_rect_pos[0], start_rect_pos[1], self._button_size[0], self._button_size[1]))
         text_continue = btn_font.render("Restart", True, (255, 255, 255))
+        text_cotinue_rect = text_continue.get_rect(center=(continue_rect.center[0] + self._win_w * 0.1,
+                                                           continue_rect.center[1] + self._win_h * 0.1))
 
         # End button
-        pygame.draw.rect(gameover_surface, (1, 1, 1), (self.end_btn_pos[0], self.end_btn_pos[1], self.button_size[0], self.button_size[1]))
+        end_rect_pos = [self.width * 0.6, self.height * 0.7]
+        end_rect = pygame.draw.rect(self.surface, (1, 1, 1), (end_rect_pos[0], end_rect_pos[1], self._button_size[0], self._button_size[1]))
         text_end = btn_font.render("End", True, (255, 255, 255))
+        text_end_rect = text_end.get_rect(center=(end_rect.center[0] + self._win_w * 0.1,
+                                                  end_rect.center[1] + self._win_h * 0.1))
         
-        
-        self._window.blit(gameover_surface, (self.surface_pos[0], self.surface_pos[1]))
-        
-        self._window.blit(text_game_over, (self.surface_pos[0] + 250, self.surface_pos[1] + 50))
+        self._window.blit(self.surface, (self.surface_pos[0], self.surface_pos[1]))
+        self._window.blit(self._img, img_rect)
+        self._window.blit(text_continue, text_cotinue_rect)
+        self._window.blit(text_end, text_end_rect)
 
-        self._window.blit(text_continue, (self.surface_pos[0] + self.start_btn_pos[0] + self.button_size[0]/4
-                                         , self.surface_pos[1] + self.start_btn_pos[1] + self.button_size[1]/4))
-
-        self._window.blit(text_end, (self.surface_pos[0] + self.end_btn_pos[0] + self.button_size[0]/4
-                                         , self.surface_pos[1] + self.end_btn_pos[1] + self.button_size[1]/4))
         pygame.display.flip()
