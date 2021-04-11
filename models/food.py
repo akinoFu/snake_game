bdfa08ba18fab907fbfbcca6950c77dd1548b691
shaniah <shaniah.nizzar@hotmail.com>
2021-04-10 18:01:13 -0700

    saving work, added Akino's changes and food (checks for overlap with  poison and apple, still need to confirm overlap with snake and apple)

--- models/food.py
+++ models/food.py
@@ -13,27 +13,40 @@ class Apple(pygame.sprite.Sprite):
         self.image = pygame.transform.scale(image, (40, 65))
         self.image.set_colorkey((255, 255, 255))
         self.rect = self.image.get_rect()
-        self.rect.x = round(random.randrange(100, 900) / 10.0) * 10.0
-        self.rect.y = round(random.randrange(100, 900) / 10.0) * 10.0
+        self.rect.x = round(random.randrange(100, 600))
+        self.rect.y = round(random.randrange(100, 600))
 
 
     def overlap_snake_poison(self, snake, poison):
-        """ Checks to see if snake head intercepts area of apple """
+        """ Checks to see if snake head intercepts area of apple and if poison collides with apple, if this happens, it changes coordinates of apple """
         if self.rect.x <= snake[0] <= self.rect.x + 40 and self.rect.y <= snake[1] <= self.rect.y + 65:
             self.rect.y = round(random.randrange(100, 600))
             self.rect.x = round(random.randrange(100, 600))
         if self.rect.colliderect(poison.rect):
-            print('in loop')
-            self.rect.x += 40
-            self.rect.y += 65
+            if self.rect.x + 80 < 750:
+                self.rect.x += 80
+            else:
+                self.rect.x -= 95
+            if self.rect.y + 95 < 750:
+                self.rect.y += 95
+            else:
+                self.rect.y -= 95
         return True
 
 
     def overlap_snake_apple(self, snake):
+        """ Checks to see if whole snake overlaps apple's coordinates, if it does, it changes apples coordinates """
         if self.rect.x <= snake['x_min'] and snake['x_max'] <= self.rect.x + 40 and self.rect.y <= snake['y_min'] and snake['y_max'] <= self.rect.y + 65:
             print('in snake overlap')
-            self.rect.x += 40
-            self.rect.y += 65
+            if self.rect.x + 80 < 750:
+                self.rect.x += 80
+            else:
+                self.rect.x -= 80
+            if self.rect.y + 95 < 750:
+                self.rect.y += 95
+            else:
+                self.rect.y -= 95
+
 
 
 class Poison(pygame.sprite.Sprite):
