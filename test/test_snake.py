2a0c72ca171eb8063a6716400586fcd24274d78a
akinoFu <akino.fu85@outlook.com>
2021-04-12 12:08:19 -0700

    Add a test file for snake related classes

---
+++ test/test_snake.py
@@ -0,0 +1,99 @@
+import pytest
+from game.models.snake import Snake, SnakeBody, SnakeHead, SnakePart
+
+@pytest.fixture
+def snake():
+    return Snake()
+
+def test_snake_init(snake):
+    """ Test the constructor of Snake class """
+    assert snake._size == 20
+    assert len(snake.full_body) == 4
+    assert snake.direction == "Left"
+
+def test_head_position(snake):
+    """ Test head_position property """
+    assert snake.head_position == [500, 500]
+
+
+def test_range(snake):
+    """ Test range property """
+    assert snake.range == {"x_min": 500,
+                           "x_max": 580,
+                           "y_min": 500,
+                           "y_max": 520
+                          }
+
+def test_move_left(snake):
+    """ Test move method (Left) """
+    snake.move()
+    assert snake._head.x == 480
+    assert snake._head.y == 500
+    assert snake._body1.x == 500
+    assert snake._body1.y == 500
+    assert snake._body2.x == 520
+    assert snake._body2.y == 500
+    assert snake._body3.x == 540
+    assert snake._body3.y == 500
+
+def test_move_up(snake):
+    """ Test move method (Up) """
+    snake.direction = "Up"
+    snake.move()
+    assert snake._head.x == 500
+    assert snake._head.y == 480
+    assert snake._body1.x == 500
+    assert snake._body1.y == 500
+    assert snake._body2.x == 520
+    assert snake._body2.y == 500
+    assert snake._body3.x == 540
+    assert snake._body3.y == 500
+
+def test_move_down(snake):
+    """ Test move method (Down) """
+    snake.direction = "Down"
+    snake.move()
+    assert snake._head.x == 500
+    assert snake._head.y == 520
+    assert snake._body1.x == 500
+    assert snake._body1.y == 500
+    assert snake._body2.x == 520
+    assert snake._body2.y == 500
+    assert snake._body3.x == 540
+    assert snake._body3.y == 500
+
+
+def test_move_right(snake):
+    """ Test move method (Right) """
+    snake.direction = "Right"
+    snake.move()
+    assert snake._head.x == 520
+    assert snake._head.y == 500
+    assert snake._body1.x == 500
+    assert snake._body1.y == 500
+    assert snake._body2.x == 520
+    assert snake._body2.y == 500
+    assert snake._body3.x == 540
+    assert snake._body3.y == 500
+
+def test_turn_Up(snake):
+    """ Test turn method (Up) """
+    snake.turn("Up")
+    assert snake.direction == "Up"
+
+def test_add_body(snake):
+    """ Test add_body method """
+    snake.add_body()
+    assert len(snake.full_body) == 5
+    assert isinstance(snake.full_body[4], SnakeBody)
+    assert snake.full_body[4].x == snake.full_body[3].x + snake._size
+    assert snake.full_body[4].y == snake.full_body[3].y
+
+def test_inheritance(snake):
+    """ Test the inheritance """
+    for part in snake.full_body:
+        assert isinstance(part, SnakePart)
+    assert isinstance(snake.full_body[0], SnakeHead)
+    assert isinstance(snake.full_body[1], SnakeBody)
+    assert isinstance(snake.full_body[2], SnakeBody)
+    assert isinstance(snake.full_body[3], SnakeBody)
